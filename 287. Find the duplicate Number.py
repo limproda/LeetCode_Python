27e8f2e20e311bd6f1a05e4e74b816eff6e51817
Luigi Michele Improda <limproda@gmail.com>
2023-09-20 15:54:57 -0500

    "Modified 287 Solution"

--- 287. Find the duplicate Number.py
+++ 287. Find the duplicate Number.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 class Solution:
+    # Approach 1
     def findDuplicate(self, nums):
         seen = set()
         for number in nums:
             if number in seen:
                 return number
             seen.add(number)
-
+            
 def main():
     sol = Solution()
     nums = [1,3,4,2,2]
